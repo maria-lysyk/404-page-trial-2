b76ceb31271d6bcb05a60fcd3748ebf087ac5d23
maria-lysyk <113712306+maria-lysyk@users.noreply.github.com>
2023-01-20 04:43:14 +0200

    Add files via upload

---
+++ tailwind.config1.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontSize: {
+        '11xl': ['14rem', { lineHeight: '1.5' }]
+      },
+      dropShadow: {
+        's': '0 9px 5px #666666',
+        }
+    },
+  },
+  plugins: [],
+}
